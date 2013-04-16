e89c355c2aaa732acdb63b090839148306b16163
Tom <tom@tom-laptop.(none)>
2013-04-16 17:23:20 +0200

    Modification compiler.sh.

--- src/compiler.sh
+++ src/compiler.sh
@@ -1,1 +1,1 @@
-bison -d -o syntsem.c syntsem.y && flex -o lexical.c lexical.lex && gcc -o syntsem syntsem.c lexical.c
+bison -d --report=state  -o syntsem.c syntsem.y && flex -o lexical.c lexical.lex && gcc -o syntsem syntsem.c lexical.c
